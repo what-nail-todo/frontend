fe8e3579055450e8b936c8da6c1854b450e58a41
조하은 <jhe93@naver.com>
2025-05-26 14:59:50 +0900

    feat: 로그인 컴포넌트에 loginApi 연결
    - useMutation을 사용하여 loginApi 요청 로직 구현
    - 폼 데이터 수집 및 mutate 함수 호출
    - 로그인 성공 시 queryClient invalidate 처리

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -2,13 +2,43 @@ import CloseIcon from './../assets/icon_x.svg';
 import { Link } from 'react-router-dom';
 import Input from '../components/Input';
 import Logo from '../components/Logo';
+import { useMutation } from '@tanstack/react-query';
+import { loginApi } from '../api/loginApi';
+import { queryClient } from '../utils/http';
+import { FormEvent } from 'react';
+
+interface LoginType {
+    email: string;
+    password: string;
+}
 
 export default function LogIn() {
+    const { mutate, isPending, isError, error } = useMutation({
+        mutationFn: loginApi,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['info'] });
+            console.log('성공했습니다.');
+
+            //navigate('/events');
+        },
+        onError: () => {},
+    });
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+
+        mutate({ email, password });
+    }
+
     return (
-        <main className="page-content flex h-screen items-center">
+        <form onSubmit={handleSubmit} className="page-content flex h-screen items-center">
             <div className="password-recovery-section flex flex-row my-0 mx-auto justify-between w-[1300px] h-[407px] items-center">
-                <section className="h-[290px] flex flex-col justify-between items-center">
-                    <div className="flex flex-col justify-between w-[500px] h-[198px] items-center">
+                <section className="h-[71.25%] flex flex-col justify-between items-center">
+                    <div className="flex flex-col justify-between w-[500px] h-[68.28%] items-center">
                         <Logo />
                         <p className="inline-block w-full text-xl text-[#5C5C5C]">
                             서비스 소개글입니다.서비스 소개글입니다.서비스 소개글입니다.서비스 소개글입니다. 서비스
@@ -26,19 +56,22 @@ export default function LogIn() {
                             <img src={CloseIcon} alt="닫기" className="w-3 h-3 " />
                         </button>
                     </div>
-                    <div className="flex flex-col justify-between h-[218px]">
-                        <Input label="이메일" type="email" placeholder="example@gmail.com" />
+                    <div className="flex flex-col justify-between h-[53.56%]">
+                        <Input label="email" type="email" placeholder="example@gmail.com" />
                         <Input
-                            label="비밀번호"
+                            label="password"
                             type="password"
                             placeholder="8자 이상, 대문자와 특수문자를 포함하세요"
                         />
                     </div>
-                    <button className="w-full h-[57px] bg-[#FB6091] text-xl text-white rounded-[6px] cursor-pointer">
+                    <button
+                        type="submit"
+                        className="w-full h-[14%] bg-[#FB6091] text-xl text-white rounded-[6px] cursor-pointer"
+                    >
                         로그인
                     </button>
                 </section>
             </div>
-        </main>
+        </form>
     );
 }
